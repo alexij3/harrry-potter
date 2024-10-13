5dafc7e2ac430f518160744c8ec0a517e496a9ea
Oleksii Buzilov <slmk445@gmail.com>
2024-10-13 19:21:56 +0300

    -- Added README.md and did refactoring

--- src/edu/pro/Main.java
+++ src/edu/pro/Main.java
@@ -2,70 +2,67 @@ package edu.pro;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Locale;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static String cleanText(String url) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(url)));
-        content = content.replaceAll("[^A-Za-z ]"," ").toLowerCase(Locale.ROOT);
-        return content;
+        Path filePath = Paths.get(url);
+        if (Files.exists(filePath)) {
+            String content = new String(Files.readAllBytes(filePath));
+            content = content.replaceAll("[^A-Za-z ]"," ").toLowerCase(Locale.ROOT);
+            return content;
+        } else {
+            throw new IOException("Файл не знайдено");
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
+        System.out.println(getExecutionTime(countWords()));
+    }
 
-        LocalDateTime start = LocalDateTime.now();
-       // Path path = Paths.get()
-        String content = new String(Files.readAllBytes(Paths.get("src/edu/pro/txt/harry.txt")));
+    private static Runnable countWords() throws IOException {
+        return () -> {
+            String[] words;
+            try {
+                words = extractWords();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-        content = content.replaceAll("[^A-Za-z ]"," ").toLowerCase(Locale.ROOT);
+            Map<String, Long> wordsCount = Arrays.stream(words)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+            wordsCount.entrySet().stream()
+                    .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                    .limit(30)
+                    .forEach(entry -> System.out.println(entry.getKey() + " " + entry.getValue()));
+
+            System.out.println("------");
+        };
+    }
+
+    private static String[] extractWords() throws IOException {
+        String content = cleanText("src/edu/pro/txt/harry.txt");
 
         String[] words = content.split(" +"); // 400 000
 
         Arrays.sort(words);
-
-        String distinctString = " ";
-
-        for (int i = 0; i < words.length ; i++) {
-            if (!distinctString.contains(words[i])){
-                distinctString+= words[i] + " ";
-            }
-        }
-
-        String[] distincts = distinctString.split(" ");
-        int[] freq = new int[distincts.length];
-
-        for (int i = 0; i < distincts.length ; i++) {
-            int count = 0;
-            for (int j = 0; j < words.length ; j++) {
-                if (distincts[i].equals(words[j])) {
-                    count++;
-                }
-            }
-            freq[i] = count;
-        }
-
-        for (int i = 0; i < distincts.length ; i++) { // 5 000
-            distincts[i] += " " + freq[i];
-        }
-
-        Arrays.sort(distincts, Comparator.comparing(str
-                -> Integer.valueOf(str.replaceAll("[^0-9]", ""))));
-
-        for (int i = 0; i < 30; i++) {
-            System.out.println(distincts[distincts.length - 1 - i]);
-        }
-        LocalDateTime finish = LocalDateTime.now();
-
-        System.out.println("------");
-        System.out.println(ChronoUnit.MILLIS.between(start, finish));
-
+        return words;
     }
+
+    public static long getExecutionTime(Runnable task) {
+        LocalDateTime start = LocalDateTime.now();
+        task.run();
+        LocalDateTime finish = LocalDateTime.now();
+        return ChronoUnit.MILLIS.between(start, finish);
+    }
+
 }
